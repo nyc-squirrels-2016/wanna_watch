25b4099962ac20f9c6f20241980acf06b4ef2991
Steven King <stevenking86@gmail.com>
2016-03-10 16:46:53 -0500

    Fix typo in migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+  validates :username, :email, presence: true
+  validates :username, :email, uniqueness: true
+
+
+
+  has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -16,18 +16,18 @@ ActiveRecord::Schema.define(version: 20160310212710) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "users_tables", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "username",        null: false
     t.string   "password_digest"
     t.string   "email",           null: false
-    t.string   "first_name"
-    t.string   "last_name"
+    t.string   "first_name",      null: false
+    t.string   "last_name",       null: false
     t.string   "about_me"
     t.string   "phone_number"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
 
-  add_index "users_tables", ["username"], name: "index_users_tables_on_username", using: :btree
+  add_index "users", ["username"], name: "index_users_on_username", using: :btree
 
 end
