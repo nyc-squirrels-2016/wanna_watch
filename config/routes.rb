3e166a1065c75cccd5f07932005644e547cfda16
Steven King <stevenking86@gmail.com>
2016-03-10 16:59:05 -0500

    Write routes and controller methods for User

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
+
+
+
+resources :users, only: [:show, :create]
+resources :sessions, only: [:create]
+
+
+
+get 'register' => 'users#new'
+get 'login' => 'sessions#new'
+get 'logout' => 'sessions#destroy'
+
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
