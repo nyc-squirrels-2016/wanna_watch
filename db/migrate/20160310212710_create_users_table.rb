cdc313c37877166436e72e191391fd5cfa2b485d
Steven King <stevenking86@gmail.com>
2016-03-10 16:35:54 -0500

    Update bundle, migrate

--- db/migrate/20160310212710_create_users_table.rb
+++ db/migrate/20160310212710_create_users_table.rb
@@ -1,6 +1,15 @@
 class CreateUsersTable < ActiveRecord::Migration
   def change
     create_table :users_tables do |t|
+      t.string :username, null: false, index: true, unique: true
+      t.string :password_digest
+      t.string :email, null: false, unique: true
+      t.string :first_name
+      t.string :last_name
+      t.string :about_me
+      t.string :phone_number, unique: true
+
+      t.timestamps null: false
     end
   end
 end
