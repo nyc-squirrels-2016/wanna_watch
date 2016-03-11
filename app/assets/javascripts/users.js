0115a0b5420cda9885a35bbfd78298c753cc8ddb
Steven King <email": null>
2016-03-11 14:32:32 -0500

    Render animated login form upon click

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -6,5 +6,17 @@ var RegisterButton = function(){
       height: 'toggle'
     });
     $('*[data-div-type="video"]').css("opacity", ".4");
+    $('*[data-div-type="register_button"]').unbind("click")
+  });
+}
+
+var LoginButton = function(){
+  $('*[data-div-type="login_partial"]').hide()
+  $('*[data-div-type="login_button"]').click(function(){
+    $('*[data-div-type="login_partial"]').animate({
+      height: 'toggle'
+    });
+    $('*[data-div-type="video"]').css("opacity", ".4");
+    $('*[data-div-type="login_button"]').unbind("click")
   });
 }
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,12 @@
 class SessionsController<ApplicationController
 
   def new
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user
+    else
+      redirect_to '/'
   end
 
   def create
