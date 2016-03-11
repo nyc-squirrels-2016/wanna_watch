4ae142aba5c734a4b00c807e7601bb273c086c02
Steven King <email": null>
2016-03-11 11:57:22 -0500

    omg javascript. clicking register renders register form animation

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,9 @@
+var RegisterButton = function(){
+  $('*[data-div-type="users_partial"]').hide()
+
+  $('*[data-div-type="register_button"]').click(function(){
+    $('*[data-div-type="users_partial"]').animate({
+      height: 'toggle'
+    });
+  });
+}
