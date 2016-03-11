19499c9966d2277efb3eda21f97f9f1fef26a7f1
Steven King <email": null>
2016-03-11 15:57:58 -0500

    make new event partial, fix some formatting in register partial, edit events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,10 +3,18 @@ class EventsController < ApplicationController
   end
 
   def new
+    @event = Event.new
+    render "_new_event", layout: false
   end
 
   def create
+    @user = current_user
     @event = Event.new(event_params)
+      if @event.save
+        redirect_to @event
+      else
+        redirect_to @user
+      end
   end
 
   def show
@@ -26,7 +34,6 @@ class EventsController < ApplicationController
   private
 
   def event_params
-    params.require(:event).permit(:shows, :time, :date, :description, :User
-      max_occupancy)
+    params.require(:event).permit(:shows, :time, :date, :description, :max_occupancy)
   end
 end
