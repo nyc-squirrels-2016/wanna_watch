abedb2e215ca5c67ec2de6c9532837e4ddff5457
Liam Mackey <liammac21@gmail.com>
2016-03-11 18:13:17 -0500

    Got Events show page working

--- app/models/event.rb
+++ app/models/event.rb
@@ -10,5 +10,14 @@ class Event < ActiveRecord::Base
   validates :description, presence: true
   validates :max_occupancy, presence: true
 
+  def attending_guests
+  	attending_guests = self.requests.where(active: 0).map do |request|
+  		request.guest
+  	end
+  	attending_guests
+  end
 
+  def filled?
+  	self.max_occupancy <= self.attending_guests.length
+  end
 end
