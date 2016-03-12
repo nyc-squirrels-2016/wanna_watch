353616c8ebeb2539e088469369557a3b2fcca968
Liam Mackey <liammac21@gmail.com>
2016-03-12 18:11:50 -0500

    Added exit buttons for forms

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -22,6 +22,45 @@ var LoginButton = function(){
 }
 
 var Unbinder = function(){
-  $('*[data-div-type="login_button"]').unbind("click")
-  $('*[data-div-type="register_button"]').unbind("click")
+  $('*[data-div-type="login_button"]').off("click")
+  $('*[data-div-type="register_button"]').off("click")
 }
+
+
+var ExitLogin = function(){
+  $('*[data-div-type="close_login_button"]').click(function(){
+    $('*[data-div-type="login_partial"]').hide()
+    $('*[data-div-type="video"]').css("opacity", "1");
+    $('*[data-div-type="login_button"]').on("click", function(){
+      $('*[data-div-type="login_partial"]').show()
+      $('*[data-div-type="video"]').css("opacity", ".4");
+    });
+    $('*[data-div-type="register_button"]').on("click", function(){
+      $('*[data-div-type="users_partial"]').show()
+      $('*[data-div-type="video"]').css("opacity", ".4");
+    });
+  })
+}
+
+var ExitRegister = function(){
+  $('*[data-div-type="close_register_button"]').click(function(){
+    $('*[data-div-type="users_partial"]').hide()
+    $('*[data-div-type="video"]').css("opacity", "1");
+    $('*[data-div-type="register_button"]').on("click", function(){
+      $('*[data-div-type="users_partial"]').show()
+      $('*[data-div-type="video"]').css("opacity", ".4");
+    });
+    $('*[data-div-type="login_button"]').on("click", function(){
+      $('*[data-div-type="login_partial"]').show()
+      $('*[data-div-type="video"]').css("opacity", ".4");
+    });
+  })
+}
+
+
+
+
+
+
+
+
