0bfb8745bdd5ec902e1a9d67fdfeda6a90a3b064
Liam Mackey <liammac21@gmail.com>
2016-03-12 14:28:23 -0500

    Got basic jquery for new forms

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,1 +1,25 @@
-events.js
+var NewEventButton = function(){
+  $('*[data-div-type="new_events_partial"]').hide()
+
+  $('*[data-div-type="new_event_button"]').click(function(){
+    $('*[data-div-type="new_events_partial"]').animate({
+      height: 'toggle'
+    });
+    Unbinder()
+  });
+}
+
+var EditEventButton = function(){
+  $('*[data-div-type="edit_events_partial"]').hide()
+  $('#events-container').on('click', '*[data-div-type="edit_event_button"]', function(){
+    $('*[data-div-type="edit_events_partial"]').animate({
+      height: 'toggle'
+    });
+    Unbinder()
+  });
+}
+
+var Unbinder = function(){
+  $('*[data-div-type="new_event_button"]').unbind("click")
+  $('*[data-div-type="edit_event_button"]').unbind("click")
+}
