7180ae52fd8ac94d6addfd37bf5e58e9671cab50
Steven King <email": null>
2016-03-12 20:05:48 -0500

    Attempt to remove all references to username

--- db/migrate/20160310212710_create_users_table.rb
+++ db/migrate/20160310212710_create_users_table.rb
@@ -1,9 +1,8 @@
 class CreateUsersTable < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :username, null: false, index: true, unique: true
       t.string :password_digest
-      t.string :email, null: false, unique: true
+      t.string :email, null: false, unique: true, index: true
       t.string :first_name, null: false
       t.string :last_name, null: false
       t.string :about_me
