c879eb4f08958dbe7fee8fd1a4123a776119a3c7
Steven King <email": null>
2016-03-15 22:00:57 -0400

    Make selected tab bold upon click on homepage

--- app/assets/javascripts/about_the_app.js
+++ app/assets/javascripts/about_the_app.js
@@ -2,6 +2,8 @@ var ShowAboutHosting = function (){
   $('*[data-div-type="host_clicker"]').click(function(){
     $('*[data-div-type="as_a_guest"]').hide()
     $('*[data-div-type="as_a_host"]').animate({height: 'toggle'})
+    $('*[data-div-type="host_clicker"]').css("font-weight", "bold")
+    $('*[data-div-type="guest_clicker"]').css("font-weight", "normal")
   });
 }
 
@@ -9,12 +11,16 @@ var ShowAboutGuesting = function (){
   $('*[data-div-type="guest_clicker"]').click(function(){
     $('*[data-div-type="as_a_host"]').hide()
     $('*[data-div-type="as_a_guest"]').animate({height: 'toggle'})
+    $('*[data-div-type="host_clicker"]').css("font-weight", "normal")
+    $('*[data-div-type="guest_clicker"]').css("font-weight", "bold")
   });
+
 }
 
 var HideAboutsOnLoad = function () {
   $('*[data-div-type="as_a_host"]').hide()
   $('*[data-div-type="as_a_guest"]').hide()
+
 }
 
 
